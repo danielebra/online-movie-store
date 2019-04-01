a2e0a065d30a5b7a70a5bb0a3389090c44153a2e
Daniel Ebrahimian <5339066+danielebra@users.noreply.github.com>
2019-04-02 10:33:19 +1100

    Create MovieCard component placeholder

---
+++ front-end/src/components/MovieCard.js
@@ -0,0 +1,10 @@
+/*
+TODO: Create a movie card component that will be used when navigating the movie store. 
+    This card is a responsive UI element
+Support:
+    - clicking (open a modal?)
+    - display image
+    - alt text
+    - title underneath? (probably not)
+
+*/
