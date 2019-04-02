f5175deca49eded4a3723f752112028d777bb16e
Daniel Ebrahimian <5339066+danielebra@users.noreply.github.com>
2019-04-02 14:22:13 +1100

    Only attach middleware if localhost (closes #4)

--- front-end/src/store.js
+++ front-end/src/store.js
@@ -1,17 +1,24 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+import {isLocalhost} from './serviceWorker';
 
 const initialState = {};
 const middleware = [thunk];
-
-const store = createStore(
+var store = createStore(
     rootReducer,
     initialState,
-    compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
 );
+// Localhost machines assumes Redux Dev Tools is available
+if (isLocalhost) {
+    store = createStore(
+        rootReducer,
+        initialState,
+        compose(
+            applyMiddleware(...middleware),
+            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        )
+    );
+}
 
 export default store;
