0886682b16a1029aae62af055a33d5b79a3eddb1
Daniel Ebrahimian <5339066+danielebra@users.noreply.github.com>
2019-04-18 20:50:22 +1000

    Add support for CORS

--- back-end/oms/settings.py
+++ back-end/oms/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['23.101.218.7',
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'movie.apps.MovieConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'oms.urls'
 
