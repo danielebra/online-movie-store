80296d2d8f2ee5bcb607e7cb8a210ecf754359db
Daniel Ebrahimian <5339066+danielebra@users.noreply.github.com>
2019-04-18 19:14:00 +1000

    Use internal redirect url as api end point

--- front-end/src/api.js
+++ front-end/src/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://23.101.218.7:8080/api/"
+  baseURL: "https://online-movie-store.netlify.com/api"
+  // baseURL: "http://23.101.218.7:8080/api/"
 });
