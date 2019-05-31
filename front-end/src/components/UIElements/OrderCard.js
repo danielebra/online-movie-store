4a300081561a55e8f2564b313cde3427c4048e7a
Daniel Ebrahimian <5339066+danielebra@users.noreply.github.com>
2019-05-31 23:09:16 +1000

    Format UIElements files

--- front-end/src/components/UIElements/OrderCard.js
+++ front-end/src/components/UIElements/OrderCard.js
@@ -2,38 +2,29 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { Link, withRouter } from "react-router-dom";
-import movie from '../../images/movie.jpg';
+import movie from "../../images/movie.jpg";
 class OrderCard extends Component {
-  constructor(props){
-    super(props)
-    this.state={
-      
-
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
   }
   render() {
-    
-
     return (
-     <div class="row">
-     <div class="col s12">
-     <div class="card red darken-3">
-     <div class="row">
-     <div className="col s4">
-     <img  src={movie} width="120" height="160"/>
-     </div>
-     <div className="col s8">
-     <div>
-       
-     </div>
-     <p >Movie Name</p>
-     </div>
-     
-     
-     </div>
-     </div>
-     </div>
-     </div>
+      <div class="row">
+        <div class="col s12">
+          <div class="card red darken-3">
+            <div class="row">
+              <div className="col s4">
+                <img src={movie} width="120" height="160" />
+              </div>
+              <div className="col s8">
+                <div />
+                <p>Movie Name</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
