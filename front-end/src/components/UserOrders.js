ebf854a4546cc3271bdbf3872ea125fff1412edd
samikhalildev <sami.khalil.dev@gmail.com>
2019-05-31 20:18:10 +1000

    Fix order editing feature

--- front-end/src/components/UserOrders.js
+++ front-end/src/components/UserOrders.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { Link, withRouter } from "react-router-dom";
@@ -16,15 +16,24 @@ class UserOrders extends Component{
     constructor() {
         super();
         this.state={
+            orders: [],
             searchDate: new Date(),
-            isEditing: false,
-            updatedQuantity: 0
+            isEditing: []
         }; 
     }
     
     componentWillMount() {
         this.props.getOrders();
-        console.log(this.props.movies.orders)
+        console.log('all orders', this.props.movies.orders)
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.movies.orders) {
+            const { orders } = nextProps.movies;
+            let isEditing = new Array(orders.length).fill(false);
+
+            this.setState({ orders, isEditing });
+        }
     }
 
     handleChange=(date)=>{
@@ -57,27 +66,45 @@ class UserOrders extends Component{
    filtering=(orders, filterDate)=>{
        return orders.filter(order => this.reformatDate(new Date(order.date.toLocaleString())) === this.reformatDate(filterDate))
    }
-   refreshEditing = (order) =>{
-       if (this.state.isEditing === false){
-        this.setState({isEditing: true})
-        
-       }else{
-           order.quantity = this.state.updatedQuantity
-           this.props.updateOrder(order)
-           this.setState({isEditing: false})
-           
-       }
-       
-   }
-   
+
+
+
+
+   switchToEditingMode(index) {
+        let arr = new Array(this.state.orders.length).fill(false);
+        arr[index] = true; 
+
+        this.state.isEditing = arr;
+        this.forceUpdate()
+    }
+
+    closeEditingMode() {
+        let arr = new Array(this.state.orders.length).fill(false);
+        this.state.isEditing = arr;
+        this.forceUpdate()
+    }
+
+    // passes the user details to redux 
+    editOrder(index) {
+        let order = this.state.orders[index];
+        console.log('editing order', order);
+
+        this.props.updateOrder(order);
+    }
+
+
+
 
    orderTable=(orders)=>{
+       
     if(this.isFilter()) {
         orders = this.filtering(orders, this.state.searchDate)
     }
+
     return( orders.length > 0 ? orders.map((order, index) => {
         let orderDate = new Date(order.date).toLocaleString()
-        console.log(order.quantity)
+        console.log(order);
+        
      return (
          (
             <tr>
@@ -93,29 +120,47 @@ class UserOrders extends Component{
                 </td>
 
                 <td>
-                <input
-                                                    type="text"
-                                                    value = {order.quantity}
-                                                    onChange={event => {
-                                                        this.setState({updatedQuantity: event.target.value})
-                                                    }
-                                                    } 
-                                                    required
-                                                    className="white-text"
-                                                    aria-required=""
-                                                    disabled = {(this.state.isEditing)? "" : "disabled"}
-                                                />
+                { this.state.isEditing[index] ? (
+                    <input
+                        type="text"
+                        value = {order.quantity}
+                        onChange={event => {
+                            orders[index].quantity = event.target.value;
+                            this.forceUpdate();
+                            }
+                        } 
+                        required
+                        className="white-text"
+                    />
+                ) : 
+                    order.quantity
+                }
+                
                 </td>
                 <td>
                     {order.total_cost}
                 </td>
                 <td>
-                    <td>
-                        <i  onclick = {() => this.refreshEditing(order)} className="material-icons pointer">{(this.state.isEditing)? "save":"edit"}</i>
-                    </td>
-                    <td>
-                    <i onClick= {() => this.handleDelete(order)} className="material-icons pointer">delete</i>
-                    </td>
+                    { this.state.isEditing[index] ? (
+                        <Fragment>
+                            <td>
+                                <i onClick = {() => this.editOrder(index)} className="material-icons pointer">save</i>
+                            </td>
+                            <td>
+                                <i onClick= {() => this.closeEditingMode()} className="material-icons pointer">cancel</i>
+                            </td>
+                        </Fragment>
+                    ) : (
+                        <Fragment>
+                            <td>
+                                <i onClick = {() => this.switchToEditingMode(index)} className="material-icons pointer">edit</i>
+                            </td>
+                            <td>
+                                <i onClick= {() => this.handleDelete(order)} className="material-icons pointer">delete</i>
+                            </td>
+                        </Fragment>
+                    )}
+                    
                 </td>
             </tr>
         ))
@@ -124,11 +169,8 @@ class UserOrders extends Component{
 }
 
     render(){
-        let { orders } = this.props.movies;
-        let orderList = "";
-        console.log(this.props.movies);
-        console.log(this.state.isEditing);
-
+        let { orders } = this.state;
+        console.log('state', this.state);
         return(
             <div className="center top-padding account-details">
                 <div className="container">
@@ -158,7 +200,6 @@ class UserOrders extends Component{
                                     </thead>
                                     <tbody>
                                             {this.orderTable(orders) }
-
                                     </tbody>
                                 </table>
                             </form>
